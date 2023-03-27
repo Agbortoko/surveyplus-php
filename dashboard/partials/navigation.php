896915471f24f652ac7c27f4bbe955c147ba18a9
Agbortoko <arreyagbortoko@gmail.com>
2023-03-27 06:21:57 +0100

    Added link to frontend from dashboard

--- dashboard/partials/navigation.php
+++ dashboard/partials/navigation.php
@@ -3,6 +3,11 @@
     <a class="navbar-brand ps-3 fw-bold fs-3" href="<?= DASHBOARD_URL ?>">survey+</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+
+    <span>
+        <a href="<?= base_url() ?>" class="btn btn-warning ms-5" target="_blank">FrontEnd</a>
+    </span>
+
     <!-- User handle-->
     <span class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0 text-light">
       Howdy! <?= (isset($_SESSION["full_name"])) ? $_SESSION["full_name"] : "User" ?>
